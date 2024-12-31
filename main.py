d7eed6ced66e8543c03f10db1485282b2a85257c
Derek Marion <github.magnify953@passmail.net>
2024-12-30 16:58:18 -0800

    Add game loop pause to limit frame rate to 60hz

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ def main():
 
     running = True
 
+    # Time
+    game_clock = pygame.time.Clock()
+    dt = 0
+
     # Game loop
     while running:
         for event in pygame.event.get():
@@ -16,6 +20,8 @@ def main():
 
         screen.fill((0, 0, 0))
         pygame.display.flip()
+
+        dt = game_clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
