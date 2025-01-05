83d079d881e0755bbafd41c0a765e7132d75d4ad
Derek Marion <github.magnify953@passmail.net>
2025-01-05 10:12:16 -0800

    Add Asteroid and AsteroidField class to randomly spawn asteroid

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 def main():
@@ -16,11 +18,18 @@ def main():
     # Group definitions
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     # Player definition
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2, PLAYER_RADIUS)
-    updatable.add(player)
-    drawable.add(player)
+
+    # Asteroid definition
+    Asteroid.containers = (asteroids, updatable, drawable)
+
+    # Asteroid field definition
+    AsteroidField.containers = updatable
+    asteroidfield = AsteroidField()
 
     # Game loop
     while running:
